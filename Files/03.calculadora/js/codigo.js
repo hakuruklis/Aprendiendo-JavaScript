b2bc37f0ca6dcdf49b3a39ca69bb9d76e5829720
Humberto Kuruklis <hakuruklis@gmail.com>
2018-10-24 13:53:36 -0500

    casi termino la calculadora xd

--- Files/03.calculadora/js/codigo.js
+++ Files/03.calculadora/js/codigo.js
@@ -3,12 +3,13 @@ OBJETO CON LAS PROPIEDADES DE LA CALCULADORA
 =============================================*/
 
 var p = {
-
-	teclas: document.querySelectorAll("#calculadora ul li")
-	accion: null;
-	digito: null;
-	operaciones: document.querySelector("#operaciones")
-
+	teclas: document.querySelectorAll("#calculadora ul li"),
+	accion: null,
+	digito: null,
+	operaciones: document.querySelector("#operaciones"),
+	cantidadSignos: 0,
+	cantdadDecimal: false,
+	resultado: false
 
 }
 
@@ -18,44 +19,74 @@ OBJETO CON LOS METODOS DE LA CALCULADORA
 
 var m = {
 
-	inicio: function(){
+	inicio: function() {
 
-		for(var i = 0; i < p.teclas.length; i++){
+		for (var i = 0; i < p.teclas.length; i++) {
 
-			p.teclas[i].addEventListener("click",m.oprimirTecla)
+			p.teclas[i].addEventListener("click", m.oprimirTecla)
 
 		}
 
 	},
 
-	oprimirTecla: function(tecla){
+	teclado: fun
+
+	oprimirTecla: function(tecla) {
 
 		p.accion = tecla.target.getAttribute("class");
 		p.digito = tecla.target.innerHTML;
 		m.calculadora(p.accion, p.operaciones);
 
 	},
-	calculadora: function(accion, digito){
-		switch(accion){
+	calculadora: function(accion, digito) {
+		switch (accion) {
 
 			case "numero":
-				p.operaciones.innerHTML += digito;
-			break;
+				p.cantidadSignos = 0;
+				if (p.operaciones.innerHTML == 0) {
+					p.operaciones.innerHTML = p.digito
+				} else {
+					if (p.resultado) {
+						p.resultado = false;
+						p.operaciones.innerHTML = p.digito
+					} else {
+						p.operaciones.innerHTML += p.digito
+					}
+				}
+				break;
 
 			case "signo":
-				console.log("signo");
-			break;
+				p.cantidadSignos++
+					if (p.cantidadSignos == 1) {
+						if (p.operaciones.innerHTML == 0) {
+							p.operaciones.innerHTML = 0
+						} else {
+							p.operaciones.innerHTML += p.digito;
+							p.cantdadDecimal = false;
+							p.resultado = false;
+						}
+					}
+				break;
 
 			case "decimal":
-				console.log("decimal");
-			break;
+				if (!p.cantdadDecimal) {
+					p.operaciones.innerHTML += p.digito;
+					p.cantdadDecimal = true;
+					p.resultado = false;
+				}
+
+				break;
 
 			case "igual":
-				console.log("igual");
-			break;
+				p.operaciones.innerHTML = eval(p.operaciones.innerHTML);
+				p.resultado = true;
+				break;
 
 		}
 
+	},
+	borrarCalculadora: function() {
+		p.operaciones.innerHTML = 0;
 	}
 }
 m.inicio();

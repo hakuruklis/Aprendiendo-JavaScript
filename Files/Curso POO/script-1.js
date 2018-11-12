ed738c4b2cb72a604f53161526994785defebfad
Humberto Kuruklis <hakuruklis@gmail.com>
2018-11-12 13:25:08 -0500

    POO

---
+++ Files/Curso POO/script-1.js
@@ -0,0 +1,35 @@
+// //Cliente de EDteam
+
+// const clienteEDteam = {
+//     nombre: "Humberto",
+//     apellido: "Kuruklis",
+//     email: "humberto@kuruklis.com",
+//     password: "sdfgjYTUY#/%&FI"
+// }
+
+// //ACCEDER A ATRIBUTOS DE UN OBJETO
+// // Sintaxis punto
+// console.log(clienteEDteam.nombre)
+
+// //Sintaxis corchetes
+// console.log(clienteEDteam["nombre"])
+
+//--------------------------------------------------------------------------
+
+//Cliente Ginmasio
+// const clienteGym = {
+//     nombre: "Humberto",
+//     edad: 28,
+//     peso: 85,
+//     altura: 175,
+//     fechaRegistro: "22/02/2018"
+// }
+// const clienteGym2 = {
+//     nombre: "Juan",
+//     edad: 35,
+//     peso: 90,
+//     altura: 190,
+//     fechaRegistro: "01/04/2018"
+// }
+
+// const clientes = [clienteGym, clienteGym2]
---
+++ Files/Curso POO/script-2.js
@@ -0,0 +1,7 @@
+let usuario = {
+    nombre : "Humberto Kuruklis",
+    email : "humberto@kuruklis.com",
+    edad : 20
+}
+
+let nombreUsusario1 = usuario["nombre"];
